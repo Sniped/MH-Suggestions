35ad5a3a4d56039397ece325dc1a2c54f8e34375
Facto <33882218+Sniped@users.noreply.github.com>
2019-03-04 18:10:33 -0500

    log smth else

--- commands/lookup.js
+++ commands/lookup.js
@@ -11,6 +11,7 @@ module.exports = {
             const channel = client.channels.get(client.config.featurechannel)
             channel.fetchMessage(data.message).then(m => {
                 const emoji = client.emojis.get('546435753719103488');
+                console.log(m.reactions);
                 const embed = new Discord.RichEmbed()
                 .setTitle(`Showing data for ${args[0]} suggestion ${data.id}`)
                 .setDescription(`**ID**: ${data.id}\n\n**Author**: ${m.author.username}\n\n**Suggestion**: ${m.content}\n\n**Upvotes**: ${data.number}\n\n**Downvotes**: ${m.reactions.get(emoji.id).count-1}`)
