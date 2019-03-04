179e76ad6190c1150db5770cc8b3eda6771d0edf
Facto <33882218+Sniped@users.noreply.github.com>
2019-03-04 17:11:40 -0500

    fix?

--- commands/lookup.js
+++ commands/lookup.js
@@ -10,7 +10,8 @@ module.exports = {
             if (!data) return msg.channel.send(':x: Invalid ID! Maybe you looked up the wrong category?');
             const channel = client.channels.get(client.config.featurechannel)
             channel.fetchMessage(data.message).then(m => {
-                m.reactions.get(client.emojis.get('546435753719103488')).then(rmsg => {
+                const no = client.emojis.find(emoji => emoji.name === 'no');
+                m.reactions.get(no).then(rmsg => {
                     const downvote = rmsg.count;
                     const m = rmsg.message;
                     const embed = new Discord.RichEmbed()
@@ -20,8 +21,7 @@ module.exports = {
                     .setColor('#388E8E');
                     msg.channel.send(embed);
                 });
-            });  
-
+            });    
         } else if (args[0] == 'discord') {
             if (!args[1]) return msg.channel.send(':x: You must include an ID.');
             if (Number(args[1]) == NaN) return msg.channel.send(':x: Your ID must be a number.');
@@ -30,7 +30,8 @@ module.exports = {
             if (!data) return msg.channel.send(':x: Invalid ID! Maybe you looked up the wrong category?');
             const channel = client.channels.get(client.config.discordchannel);
             channel.fetchMessage(data.message).then(m => {
-                m.reactions.get(client.emojis.get('546435753719103488')).then(rmsg => {
+                const no = client.emojis.find(emoji => emoji.name === 'no');
+                m.reactions.get(no).then(rmsg => {
                     const downvote = rmsg.count;
                     const m = rmsg.message;
                     const embed = new Discord.RichEmbed()
@@ -41,7 +42,6 @@ module.exports = {
                     msg.channel.send(embed);
                 });
             });  
-    
         } else if (args[0] == 'event') {
             if (!args[1]) return msg.channel.send(':x: You must include an ID.');
             if (Number(args[1]) == NaN) return msg.channel.send(':x: Your ID must be a number.');
@@ -50,7 +50,8 @@ module.exports = {
             if (!data) return msg.channel.send(':x: Invalid ID! Maybe you looked up the wrong category?');
             const channel = client.channels.get(client.config.eventchannel);
             channel.fetchMessage(data.message).then(m => {
-                m.reactions.get(client.emojis.get('546435753719103488')).then(rmsg => {
+                const no = client.emojis.find(emoji => emoji.name === 'no');
+                m.reactions.get(no).then(rmsg => {
                     const downvote = rmsg.count;
                     const m = rmsg.message;
                     const embed = new Discord.RichEmbed()
@@ -60,8 +61,7 @@ module.exports = {
                     .setColor('#388E8E');
                     msg.channel.send(embed);
                 });
-            });  
-  
+            });    
         } else if (args[0] == 'plugin') {
             if (!args[1]) return msg.channel.send(':x: You must include an ID.');
             if (Number(args[1]) == NaN) return msg.channel.send(':x: Your ID must be a number.');
@@ -70,7 +70,8 @@ module.exports = {
             if (!data) return msg.channel.send(':x: Invalid ID! Maybe you looked up the wrong category?');
             const channel = client.channels.get(client.config.pluginchannel);
             channel.fetchMessage(data.message).then(m => {
-                m.reactions.get(client.emojis.get('546435753719103488')).then(rmsg => {
+                const no = client.emojis.find(emoji => emoji.name === 'no');
+                m.reactions.get(no).then(rmsg => {
                     const downvote = rmsg.count;
                     const m = rmsg.message;
                     const embed = new Discord.RichEmbed()
