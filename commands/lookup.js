ab31bef19b110def6f48d73a0e70136c4585ed1c
Facto <33882218+Sniped@users.noreply.github.com>
2019-03-04 17:52:52 -0500

    try to do smth

--- commands/lookup.js
+++ commands/lookup.js
@@ -11,13 +11,9 @@ module.exports = {
             const channel = client.channels.get(client.config.featurechannel)
             channel.fetchMessage(data.message).then(m => {
                 const emoji = client.emojis.get('546435753719103488');
-                console.log(emoji);
-                const rmsg = m.reactions.get(emoji.id);
-                console.log(rmsg);
-                const downvote = rmsg.count;
                 const embed = new Discord.RichEmbed()
                 .setTitle(`Showing data for ${args[0]} suggestion ${data.id}`)
-                .setDescription(`**ID**: ${data.id}\n\n**Author**: ${m.author.username}\n\n**Suggestion**: ${m.content}\n\n**Upvotes**: ${data.number}\n\n**Downvotes**: ${downvote-1}`)
+                .setDescription(`**ID**: ${data.id}\n\n**Author**: ${m.author.username}\n\n**Suggestion**: ${m.content}\n\n**Upvotes**: ${data.number}\n\n**Downvotes**: ${m.reactions.get(emoji.id).count-1}`)
                 .setThumbnail(m.author.avatarURL)
                 .setColor('#388E8E');
                 msg.channel.send(embed);
