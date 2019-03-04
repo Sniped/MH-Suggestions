85dd1037e1ecea0f963714b52ad84f6f73a08d89
Facto <33882218+Sniped@users.noreply.github.com>
2019-03-03 22:14:27 -0500

    testing

--- commands/resign.js
+++ commands/resign.js
@@ -4,6 +4,7 @@ module.exports = {
         const council_role = msg.guild.roles.find(role => role.id === client.config.councilid);
         // we need to call the message author a member of the MH Suggestions guild
         const member = client.guilds.get('546414872196415501').members.get(msg.author.id);
+        if (member) return msg.channel.send('hi');
         if (!member.roles.has(council_role.id)) return msg.channel.send(':x: You\'re not apart of the council!');
         member.removeRole(council_role.id);
         msg.channel.send(':white_check_mark: You have successfully resigned from the council team!');        
