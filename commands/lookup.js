47e8c293708de8e3b16f4da51f8ba77062de9201
Facto <33882218+Sniped@users.noreply.github.com>
2019-03-04 16:59:54 -0500

    fix?

--- commands/lookup.js
+++ commands/lookup.js
@@ -10,14 +10,16 @@ module.exports = {
             if (!data) return msg.channel.send(':x: Invalid ID! Maybe you looked up the wrong category?');
             const channel = client.channels.get(client.config.featurechannel)
             channel.fetchMessage(data.message).then(m => {
-                const downvote = m.reactions.get(client.emojis.get('546435753719103488'))
-                console.log(downvote)
-                const embed = new Discord.RichEmbed()
-                .setTitle(`Showing data for ${args[0]} suggestion ${data.id}`)
-                .setDescription(`**ID**: ${data.id}\n\n**Author**: ${m.author.username}\n\n**Suggestion**: ${m.content}\n\n**Upvotes**: ${data.number}\n\n**Downvotes**: ${downvote-1}`)
-                .setThumbnail(m.author.avatarURL)
-                .setColor('#388E8E');
-                msg.channel.send(embed);
+                m.reactions.get(client.emojis.get('546435753719103488')).then(rmsg => {
+                    const downvote = rmsg.count;
+                    const m = rmsg.message;
+                    const embed = new Discord.RichEmbed()
+                    .setTitle(`Showing data for ${args[0]} suggestion ${data.id}`)
+                    .setDescription(`**ID**: ${data.id}\n\n**Author**: ${m.author.username}\n\n**Suggestion**: ${m.content}\n\n**Upvotes**: ${data.number}\n\n**Downvotes**: ${downvote-1}`)
+                    .setThumbnail(m.author.avatarURL)
+                    .setColor('#388E8E');
+                    msg.channel.send(embed);
+                })
             });
         } else if (args[0] == 'discord') {
             if (!args[1]) return msg.channel.send(':x: You must include an ID.');
@@ -27,14 +29,17 @@ module.exports = {
             if (!data) return msg.channel.send(':x: Invalid ID! Maybe you looked up the wrong category?');
             const channel = client.channels.get(client.config.discordchannel);
             channel.fetchMessage(data.message).then(m => {
-                const downvote = m.reactions.get(client.emojis.get('546435753719103488')).count;
-                const embed = new Discord.RichEmbed()
-                .setTitle(`Showing data for ${args[0]} suggestion ${data.id}`)
-                .setDescription(`**ID**: ${data.id}\n\n**Author**: ${m.author.username}\n\n**Suggestion**: ${m.content}\n\n**Upvotes**: ${data.number}\n\n**Downvotes**: ${downvote-1}`)
-                .setThumbnail(m.author.avatarURL)
-                .setColor('#388E8E');
-                msg.channel.send(embed);
-            });           
+                m.reactions.get(client.emojis.get('546435753719103488')).then(rmsg => {
+                    const downvote = rmsg.count;
+                    const m = rmsg.message;
+                    const embed = new Discord.RichEmbed()
+                    .setTitle(`Showing data for ${args[0]} suggestion ${data.id}`)
+                    .setDescription(`**ID**: ${data.id}\n\n**Author**: ${m.author.username}\n\n**Suggestion**: ${m.content}\n\n**Upvotes**: ${data.number}\n\n**Downvotes**: ${downvote-1}`)
+                    .setThumbnail(m.author.avatarURL)
+                    .setColor('#388E8E');
+                    msg.channel.send(embed);
+                })
+            });        
         } else if (args[0] == 'event') {
             if (!args[1]) return msg.channel.send(':x: You must include an ID.');
             if (Number(args[1]) == NaN) return msg.channel.send(':x: Your ID must be a number.');
@@ -43,13 +48,16 @@ module.exports = {
             if (!data) return msg.channel.send(':x: Invalid ID! Maybe you looked up the wrong category?');
             const channel = client.channels.get(client.config.eventchannel);
             channel.fetchMessage(data.message).then(m => {
-                const downvote = m.reactions.get(client.emojis.get('546435753719103488')).count;
-                const embed = new Discord.RichEmbed()
-                .setTitle(`Showing data for ${args[0]} suggestion ${data.id}`)
-                .setDescription(`**ID**: ${data.id}\n\n**Author**: ${m.author.username}\n\n**Suggestion**: ${m.content}\n\n**Upvotes**: ${data.number}\n\n**Downvotes**: ${downvote-1}`)
-                .setThumbnail(m.author.avatarURL)
-                .setColor('#388E8E');
-                msg.channel.send(embed);
+                m.reactions.get(client.emojis.get('546435753719103488')).then(rmsg => {
+                    const downvote = rmsg.count;
+                    const m = rmsg.message;
+                    const embed = new Discord.RichEmbed()
+                    .setTitle(`Showing data for ${args[0]} suggestion ${data.id}`)
+                    .setDescription(`**ID**: ${data.id}\n\n**Author**: ${m.author.username}\n\n**Suggestion**: ${m.content}\n\n**Upvotes**: ${data.number}\n\n**Downvotes**: ${downvote-1}`)
+                    .setThumbnail(m.author.avatarURL)
+                    .setColor('#388E8E');
+                    msg.channel.send(embed);
+                })
             });
         } else if (args[0] == 'plugin') {
             if (!args[1]) return msg.channel.send(':x: You must include an ID.');
@@ -59,13 +67,16 @@ module.exports = {
             if (!data) return msg.channel.send(':x: Invalid ID! Maybe you looked up the wrong category?');
             const channel = client.channels.get(client.config.pluginchannel);
             channel.fetchMessage(data.message).then(m => {
-                const downvote = m.reactions.get(client.emojis.get('546435753719103488')).count;
-                const embed = new Discord.RichEmbed()
-                .setTitle(`Showing data for ${args[0]} suggestion ${data.id}`)
-                .setDescription(`**ID**: ${data.id}\n\n**Author**: ${m.author.username}\n\n**Suggestion**: ${m.content}\n\n**Upvotes**: ${data.number}\n\n**Downvotes**: ${downvote-1}`)
-                .setThumbnail(m.author.avatarURL)
-                .setColor('#388E8E');
-                msg.channel.send(embed);
+                m.reactions.get(client.emojis.get('546435753719103488')).then(rmsg => {
+                    const downvote = rmsg.count;
+                    const m = rmsg.message;
+                    const embed = new Discord.RichEmbed()
+                    .setTitle(`Showing data for ${args[0]} suggestion ${data.id}`)
+                    .setDescription(`**ID**: ${data.id}\n\n**Author**: ${m.author.username}\n\n**Suggestion**: ${m.content}\n\n**Upvotes**: ${data.number}\n\n**Downvotes**: ${downvote-1}`)
+                    .setThumbnail(m.author.avatarURL)
+                    .setColor('#388E8E');
+                    msg.channel.send(embed);
+                })
             });
         } else return msg.channel.send(':x: Invalid arguments! Valid arguments are `feature`, `discord`, `event` and `plugin`.')    
     },
