faa7d6fdd595891048e2f3df549354622b3d7b7c
Facto <33882218+Sniped@users.noreply.github.com>
2019-04-11 16:35:59 -0400

    args 1 is no longer needed

--- commands/settings.js
+++ commands/settings.js
@@ -2,7 +2,6 @@ module.exports = {
     run: async (client, msg, args) => {
         if (!args[0]) return msg.channel.send(':x: Invalid args! Valid arguments are `ban-msg`');
         if (args[0] == 'ban-msg') {
-            if (!args[1]) return msg.channel.send(':x: You must provide a boolean for whether you want ban messages or not.');
             const userData = await client.db.table('userData').get(msg.author.id).run();
             if (userData.notifications.banmsg == true) {
                 client.db.table('userData').get(msg.author.id).update({ notifications: { banmsg: false } }).run();
